bce9e35c2dab4b5350cc9631171f00396d1fe8fe
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-06-08 13:20:46 +0300

    Add check when to show 'subsidiary' sidebar.

--- sidebar-subsidiary.php
+++ sidebar-subsidiary.php
@@ -1,15 +1,24 @@
-<?php if ( is_active_sidebar( 'subsidiary' ) ) { ?>
+<?php	
 
-	<aside id="sidebar-subsidiary" class="sidebar" role="complementary">
+/* When to show header at all. */
+$kulkuri_show_sidebar_subsidiary = apply_filters( 'kulkuri_show_sidebar_subsidiary', true );
+	
+if( $kulkuri_show_sidebar_subsidiary ) : // Check do we show sidebar subsidiary.
+
+	if ( is_active_sidebar( 'subsidiary' ) ) : ?>
+
+		<aside id="sidebar-subsidiary" class="sidebar" role="complementary">
 		
-		<div class="wrap">
-			<div class="wrap-inside">
+			<div class="wrap">
+				<div class="wrap-inside">
 			
-				<?php dynamic_sidebar( 'subsidiary' ); ?>
+					<?php dynamic_sidebar( 'subsidiary' ); ?>
 		
-			</div><!-- .wrap-inside -->	
-		</div><!-- .div -->
+				</div><!-- .wrap-inside -->	
+			</div><!-- .div -->
 
-	</aside><!-- #sidebar-subsidiary .sidebar -->
+		</aside><!-- #sidebar-subsidiary .sidebar -->
+	
+	<?php endif; // End check for sidebar. ?>
 
-<?php } ?>
+<?php endif; // End check for when to show show sidebar subsidiary. ?>

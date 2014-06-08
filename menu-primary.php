9714f1e63ff5077d801d1f54e983ed42b77162ff
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-06-08 13:20:02 +0300

    Use only one check.

--- menu-primary.php
+++ menu-primary.php
@@ -2,22 +2,9 @@
 
 	/* Decide which menu to use. */
 	if( is_page_template( 'pages/front-page.php' ) ) :
-		$kulkuri_menu_args = array (	
-			'theme_location'  => 'primary',
-			'menu_id'         => 'menu-primary-items',
-			'depth'           => 1,
-			'menu_class'      => 'menu-items',
-			'fallback_cb'     => '',
-			//'walker'          => new kulkuri_walker()
-		);
+		$kulkuri_location = 'primary';
 	else :
-		$kulkuri_menu_args = array (	
-			'theme_location'  => 'not-primary',
-			'menu_id'         => 'menu-primary-items',
-			'depth'           => 1,
-			'menu_class'      => 'menu-items',
-			'fallback_cb'     => ''
-		);
+		$kulkuri_location = 'not-primary';
 	endif;
 
 	?>
@@ -26,7 +13,15 @@
 		<nav id="menu-primary" class="menu main-navigation nav-collapse" role="navigation">	
 			<div class="wrap">
 			
-				<?php wp_nav_menu( $kulkuri_menu_args ); ?>
+				<?php
+				wp_nav_menu( array (	
+						'theme_location'  => $kulkuri_location,
+						'menu_id'         => 'menu-primary-items',
+						'depth'           => 1,
+						'menu_class'      => 'menu-items',
+						'fallback_cb'     => ''
+				) );
+				?>
 		
 			</div><!-- .wrap -->
 		</nav><!-- #menu-primary -->

96410d630dd791d27105c48dc2f3da958f0a8133
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-06-08 13:19:24 +0300

    Remove kulkuri_categorized_blog().

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -78,47 +78,6 @@ function kulkuri_posted_on() {
 endif;
 
 /**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
-function kulkuri_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'kulkuri_categories' ) ) ) {
-		// Create an array of all the categories that are attached to posts.
-		$all_the_cool_cats = get_categories( array(
-			'fields'     => 'ids',
-			'hide_empty' => 1,
-
-			// We only need to know if there is more than one category.
-			'number'     => 2,
-		) );
-
-		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( 'kulkuri_categories', $all_the_cool_cats );
-	}
-
-	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so kulkuri_categorized_blog should return true.
-		return true;
-	} else {
-		// This blog has only 1 category so kulkuri_categorized_blog should return false.
-		return false;
-	}
-}
-
-/**
- * Flush out the transients used in kulkuri_categorized_blog.
- */
-function kulkuri_category_transient_flusher() {
-	// Like, beat it. Dig?
-	delete_transient( 'kulkuri_categories' );
-}
-add_action( 'edit_category', 'kulkuri_category_transient_flusher' );
-add_action( 'save_post',     'kulkuri_category_transient_flusher' );
-
-/**
  * This template tag is meant to replace template tags like `the_category()`, `the_terms()`, etc.  These core 
  * WordPress template tags don't offer proper translation and RTL support without having to write a lot of 
  * messy code within the theme's templates.  This is why theme developers often have to resort to custom 

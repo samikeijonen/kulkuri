c0cf0f51f7a3182bb5b3f7a2e20ed0eac2185247
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-06-19 13:21:13 +0300

    Update store url to live site.

--- theme-updater/theme-updater.php
+++ theme-updater/theme-updater.php
@@ -14,11 +14,11 @@ if ( !class_exists( 'EDD_Theme_Updater_Admin' ) ) {
 $updater = new EDD_Theme_Updater_Admin(
 
 	array(
-		'remote_api_url' => 'http://localhost/foxnet-themes-shop', // Site where EDD is hosted
-		'item_name'      => 'Kulkuri',                             // Name of theme
-		'theme_slug'     => 'kulkuri',                             // Theme slug
-		'version'        => KULKURI_VERSION,                       // The current version of this theme
-		'author'         => 'Sami Keijonen'                        // The author of this theme
+		'remote_api_url' => 'http://foxnet-themes.fi', // Site where EDD is hosted
+		'item_name'      => 'Kulkuri',                 // Name of theme
+		'theme_slug'     => 'kulkuri',                 // Theme slug
+		'version'        => KULKURI_VERSION,           // The current version of this theme
+		'author'         => 'Sami Keijonen'            // The author of this theme
 	)
 
 );

e729948fe3712180c77e06a69b43bbb23f5e2f8e
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-09-28 12:55:03 +0300

    Remote updater URI to foxland.fi.

--- theme-updater/theme-updater.php
+++ theme-updater/theme-updater.php
@@ -14,7 +14,7 @@ if ( !class_exists( 'EDD_Theme_Updater_Admin' ) ) {
 $updater = new EDD_Theme_Updater_Admin(
 
 	array(
-		'remote_api_url' => 'http://foxnet-themes.fi', // Site where EDD is hosted
+		'remote_api_url' => 'http://foxland.fi',       // Site where EDD is hosted
 		'item_name'      => 'Kulkuri',                 // Name of theme
 		'theme_slug'     => 'kulkuri',                 // Theme slug
 		'version'        => KULKURI_VERSION,           // The current version of this theme

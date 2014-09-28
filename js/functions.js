c08c4a682a20e4f867a5f8e80e7a6a494b354669
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-09-28 12:53:34 +0300

    Tab support for dropdown menus using vanilla JS. Also add skip-link-focus in the same file.

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,69 @@
 /**
- * Theme functions file
- *
- * Contains handlers for navigation accessibility.
- *
+ * Enable tab support for dropdown menus.
  */
-( function( $ ) {
-	var body    = $( 'body' ),
-		_window = $( window );
+( function() {
+	var container, menu, links;
 
-	$( function() {
-	
-		// Focus styles for menus.
-		$( '.main-navigation' ).find( 'a' ).on( 'focus blur', function() {
-			$( this ).parents().toggleClass( 'focus' );
-		} );
-		$( '.nav-toggle' ).on( 'focus blur', function() {
-			$( this ).toggleClass( 'focus' );
-		} );
-	} );
-} )( jQuery );
+	container = document.getElementById( 'menu-primary' );
+	if ( ! container )
+		return;
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	/**
+	 * Make dropdown menus keyboard accessible.
+	 */
+
+	// Get all the link elements within the menu.
+	links = menu.getElementsByTagName( 'a' );
+
+	// Each time a menu link is focused or blurred call the function toggleFocus.
+	for ( var i = 0, len = links.length; i < len; i++ ) {
+		links[i].onfocus = toggleFocus;
+		links[i].onblur = toggleFocus;
+	}
+
+	function toggleFocus() {
+		var current = this,
+		    ancestors = [];
+
+		// Create an array of <li> ancestors of the current link. Stop upon
+		// reaching .menu-items at the top of the current menu system.
+		while ( -1 === current.className.indexOf( 'menu-items' ) ) {
+			if ( 'li' === current.tagName.toLowerCase() ) {
+				ancestors.unshift( current );
+			}
+			current = current.parentElement;
+		}
+
+		// For each element in ancestors[] toggle the class .focus.
+		for ( i = 0, len = ancestors.length; i < len; i++ ) {
+			if ( -1 !== ancestors[i].className.indexOf( 'focus' ) )
+				ancestors[i].className = ancestors[i].className.replace( ' focus', '' );
+			else
+				ancestors[i].className += ' focus';
+		}
+	}
+} )();
+
+/**
+ * Skip link focus fix.
+ */
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
+		window.addEventListener( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
